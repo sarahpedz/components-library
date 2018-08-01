14fea61710fe069cb6332ab965bcb3dea89247fd
Sarah Pedley <pedleysarah@gmail.com>
2018-08-01 16:21:44 +0800

    Base wordpress update

--- wordpress-theme/themes/base/page.php
+++ wordpress-theme/themes/base/page.php
@@ -23,45 +23,45 @@ get_header();
 			the_post(); ?>
 
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+				<header class="entry-header">
+					<?php
+					if ( is_singular() ) :
+						the_title( '<h1 class="entry-title">', '</h1>' );
+					else :
+						the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+					endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+					if ( 'post' === get_post_type() ) :
+						?>
+						
+					<?php endif; ?>
+				</header><!-- .entry-header -->
 
 
-	<div class="entry-content">
-		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'base' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
+				<div class="entry-content">
+					<?php
+					the_content( sprintf(
+						wp_kses(
+							/* translators: %s: Name of current post. Only visible to screen readers */
+							__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'base' ),
+							array(
+								'span' => array(
+									'class' => array(),
+								),
+							)
+						),
+						get_the_title()
+					) );
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'base' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'base' ),
+						'after'  => '</div>',
+					) );
+					?>
+				</div><!-- .entry-content -->
 
-	
-</article><!-- #post-<?php the_ID(); ?> -->
+				
+			</article><!-- #post-<?php the_ID(); ?> -->
 
 		<?php endwhile; // End of the loop.
 		?>
